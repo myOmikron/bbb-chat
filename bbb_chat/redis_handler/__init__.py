1a3d7976ae4ed592e9ca1dcdaa29054c926a90ee
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-02-24 19:33:36 +0100

    Cached Chat model to decrease db load

--- bbb_chat/redis_handler/__init__.py
+++ bbb_chat/redis_handler/__init__.py
@@ -1,8 +1,13 @@
-from django.core.exceptions import MiddlewareNotUsed
+import time
+from threading import Thread
+
+from django.core.exceptions import MiddlewareNotUsed, AppRegistryNotReady
+from django.apps.registry import apps
 
 from redis_handler.redis import startup, register_handler
 from redis_handler.join_leave import on_join, on_leave
 from redis_handler.chat_message import on_chat_msg, Worker
+from redis_handler.state import State
 
 
 class RedisStartupMiddleware:
@@ -11,5 +16,17 @@ class RedisStartupMiddleware:
         register_handler("UserLeftMeetingEvtMsg", on_leave)
         register_handler("GroupChatMessageBroadcastEvtMsg", on_chat_msg)
         workers = [Worker() for i in range(1)]
+        StateLoader().start()
         startup()
         raise MiddlewareNotUsed("Good.Design")
+
+
+class StateLoader(Thread):
+    def run(self):
+        while True:
+            try:
+                apps.check_models_ready()
+                State.instance = State()
+                break
+            except AppRegistryNotReady:
+                time.sleep(1)
