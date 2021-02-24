899a2d8d9c15b1c8cb18d2f80056683d23e26280
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-02-24 19:34:24 +0100

    Simplified Chat model; only meeting_id needs to be unique

--- bbb_chat/api/models.py
+++ bbb_chat/api/models.py
@@ -2,12 +2,8 @@ from django.db import models
 
 
 class Chat(models.Model):
-    meeting_id = models.CharField(max_length=255, default="")
+    meeting_id = models.CharField(max_length=255, default="", unique=True)
     chat_user_name = models.CharField(max_length=255, default="")
     chat_user_id = models.CharField(max_length=255, blank=True, null=True)
     callback_uri = models.CharField(max_length=255, default="")
     callback_secret = models.CharField(max_length=255, default="")
-
-    class Meta:
-        unique_together = [("meeting_id", "chat_user_name"),
-                           ("meeting_id", "chat_user_id")]
