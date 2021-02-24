3a51d1170b418f7622158916f805971026e91e2b
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-02-24 17:54:00 +0100

    Adde chat model

--- bbb_chat/api/models.py
+++ bbb_chat/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    meeting_id = models.CharField(max_length=255, default="")
+    user_name = models.CharField(max_length=255, default="")
+    user_id = models.CharField(max_length=255, blank=True, null=True)
+    callback_uri = models.CharField(max_length=255, default="")
+    callback_secret = models.CharField(max_length=255, default="")
+
+    class Meta:
+        unique_together = ("meeting_id", "user_name")
