22b011fd4d95be891e3940f1525dc87739ec6a6c
myOmikron <accounts@omikron.dev>
2021-02-24 15:03:44 +0100

    Set few security relevant parameters

--- bbb_chat/bbb_chat/settings.py
+++ bbb_chat/bbb_chat/settings.py
@@ -20,13 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4a5(-$%zsee_u!a8r1nx7df_(o)))2^55zfgf4^!8@sniw4op9'
+SECRET_KEY = 'change_me'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+]
 
 # Application definition
 
