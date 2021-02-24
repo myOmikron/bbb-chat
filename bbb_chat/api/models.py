950cb7f5928bbbea49282c79536aa3e6411479d9
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-02-24 18:18:10 +0100

    Refactored names for consistency

--- bbb_chat/api/models.py
+++ bbb_chat/api/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 class Chat(models.Model):
     meeting_id = models.CharField(max_length=255, default="")
-    user_name = models.CharField(max_length=255, default="")
-    user_id = models.CharField(max_length=255, blank=True, null=True)
+    chat_user_name = models.CharField(max_length=255, default="")
+    chat_user_id = models.CharField(max_length=255, blank=True, null=True)
     callback_uri = models.CharField(max_length=255, default="")
     callback_secret = models.CharField(max_length=255, default="")
 
     class Meta:
-        unique_together = ("meeting_id", "user_name")
+        unique_together = [("meeting_id", "chat_user_name"),
+                           ("meeting_id", "chat_user_id")]
--- bbb_chat/redis_handler/join_leave.py
+++ bbb_chat/redis_handler/join_leave.py
@@ -10,9 +10,9 @@ def on_join(header, body):
     try:
         chat = Chat.objects.get(
             meeting_id=header["meetingId"],
-            user_name=body["name"]
+            chat_user_name=body["name"]
         )
-        chat.user_id = header["userId"]
+        chat.chat_user_id = header["userId"]
         chat.save()
     except Chat.DoesNotExist:
         logger.debug("Ignoring joining user "+body["name"]+" in meeting "+header["meetingId"])
@@ -22,9 +22,9 @@ def on_leave(header, _):
     try:
         chat = Chat.objects.get(
             meeting_id=header["meetingId"],
-            user_id=header["userId"]
+            chat_user_id=header["userId"]
         )
-        chat.user_id = None
+        chat.chat_user_id = None
         chat.save()
     except Chat.DoesNotExist:
         logger.debug("Ignoring leaving user "+header["userId"]+" in meeting "+header["meetingId"])
