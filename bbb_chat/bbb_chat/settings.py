6bc156f1c74aafb6622c4fcedf5455f7074c04e4
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-02-24 19:04:29 +0100

    Added redis startup to settings

--- bbb_chat/bbb_chat/settings.py
+++ bbb_chat/bbb_chat/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'redis_handler.RedisStartupMiddleware',
 ]
 
 ROOT_URLCONF = 'bbb_chat.urls'
